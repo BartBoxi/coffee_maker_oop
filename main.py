441cd7ef0c6524c4bcac3df3ed4c10b758d01d00
bartoszpudlo <bartoszpudlo07@gmail.com>
2023-03-22 23:05:30 +0100

    Coffee machine code with OOP

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ from money_machine import MoneyMachine
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
-menu_item = MenuItem()
-
 
 is_on = True
 
@@ -25,7 +23,14 @@ while is_on:
     options = menu.get_items()
     choice = input(f"What would you like ? ({options}) ")
     if choice == "off":
+        is_on = False
+    elif choice == "report":
         coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
+
 
 
 
